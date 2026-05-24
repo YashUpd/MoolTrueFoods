8172a29cf677cd1770bbc1796768682a82417eaf
YashUpd <yash20upd@gmail.com>
2026-05-24 18:29:10 +0530

    fix: whitelist production domains in CORS configuration

--- server/src/index.js
+++ server/src/index.js
@@ -14,6 +14,8 @@ const PORT = process.env.PORT || 5000
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
+  'https://mooltruefoods.in',
+  'https://www.mooltruefoods.in',
   process.env.CLIENT_URL,
 ].filter(Boolean)
 
